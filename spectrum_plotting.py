424955c4c2846691a93fd3193851d5a5c7080b6a
Marjolaine28 <46906318+Marjolaine28@users.noreply.github.com>
2019-05-30 17:54:49 -0400

    Update spectrum_plotting.py

--- spectrum_plotting.py
+++ spectrum_plotting.py
@@ -10,8 +10,8 @@ args = parser.parse_args()
 
 
 if __name__ == "__main__":
-	sc= np.transpose(np.load("spectra_corrected_"+args.k+"-mers.npy"))
-	s= np.transpose(np.load("spectra_"+args.k+"-mers.npy"))
+	sc= np.transpose(np.load("spectrum_corrected_"+args.k+"-mers.npy"))
+	s= np.transpose(np.load("spectrum_"+args.k+"-mers.npy"))
 	max=np.load("min-max_"+args.k+"-mers.npy")[1]
 	plt.xlim(0,max[0]*3)
 	plt.ylim(0,max[1]*1.5)
@@ -22,5 +22,5 @@ if __name__ == "__main__":
 	plt.ylabel('Frequency')
 	plt.legend()
 	plt.tight_layout()
-	plt.savefig("spectra_"+args.k+"-mers.png")
+	plt.savefig("spectrum_"+args.k+"-mers.png")
 
